f48f643275a162895fcacb4bab24a13a86bf097c
Owain Lewis <owain@owainlewis.com>
2014-10-14 19:54:19 +0100

    updates stuff

--- README.md
+++ README.md
@@ -2,9 +2,29 @@
 
 ## Notes
 
+An expression in lambda calculus can be a:
+
+variable: x
+lambda abstraction (function): λx.x
+(where . separates the function argument and body)
+function application: x y
+
+Each lambda takes a single argument and multiple arguments are obtained by nesting lambdas (λx.(λy. x y))
+
 Annonymous functions (λ v . e)
 
 The identity functions (λ x . x)
+
+## Numbers
+
+0 = λs.λz. z
+1 = λs.λz. s z
+2 = λs.λz. s (s z)
+
+## Booleans
+
+true = λt.λf.t
+false = λt.λf.f
 
 ```scheme
 (lambda (x) x)
--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,8 +1,36 @@
 module Lambda ( ) where
 
-data Expression = Literal String
-                | Lambda String Expression
-                | Apply Expression Expression
+import Text.Parsec
+import Text.Parsec.String
+
+type Identifier = String
+
+data LambdaExpr = Variable Char
+                | Abstraction Char LambdaExpr
+                | Application LambdaExpr LambdaExpr
                 deriving Show
 
-type Context = [(String, Expression)]
+lambdaExprParser :: Parser LambdaExpr
+lambdaExprParser = do char '\\'
+                      var <- letter
+                      char '.'
+                      expr <- lambdaExprParser
+                      return $ Abstraction var expr
+               <|> do apps <- many1 term
+                      return $ foldl1 Application apps
+
+term :: Parser LambdaExpr
+term = do var <- letter
+          return $ Variable var
+   <|> do char '('
+          expr <- lambdaExprParser
+          char ')'
+          return expr
+
+lC :: Parser LambdaExpr
+lC = term <|> lambdaExprParser
+
+--go input = parse (lambdaCalculus input) " "
+test input = parseTest (do expr <- lC; eof; return expr) input
+
+x = "\\y.y(\\x.x)y"
