57d3b4a13588fa3e01e0f4847d5c4230a75141e7
Owain Lewis <owain@owainlewis.com>
2014-11-16 09:06:32 +0000

    morning experiements

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,41 +1,54 @@
-module Lambda ( ) where
+{-# LANGUAGE FlexibleContexts #-}
+module Lambda
+  ( Term(..)
+  ) where
 
+import           Control.Applicative ((*>), (<*))
 import           Text.Parsec
 import           Text.Parsec.String
 
 type Identifier = String
 
-data LambdaExpr = Variable Char
-                | Abstraction Char LambdaExpr
-                | Application LambdaExpr LambdaExpr
-                deriving Show
+-- <expresson> ::= <name> | <function> | <application>
 
-lambdaExprParser :: Parser LambdaExpr
+data Term = Var String
+          | Function String Term
+          | Application Term Term
+            deriving ( Show, Eq )
+
+lexeme :: Stream s m Char =>
+          ParsecT s u m a ->
+          ParsecT s u m a
+lexeme p = spaces *> p <* spaces
+
+lambdaExprParser :: Parser Term
 lambdaExprParser = do char '\\'
-                      var <- letter
-                      char '.'
+                      var <- many1 letter
+                      lexeme $ char '.'
                       expr <- lambdaExprParser
-                      return $ Abstraction var expr
+                      return $ Function var expr
                <|> do apps <- many1 term
                       return $ foldl1 Application apps
 
-term :: Parser LambdaExpr
-term = do var <- letter
-          return $ Variable var
-   <|> do char '('
-          expr <- lambdaExprParser
-          char ')'
-          return expr
+parseVar :: Parser Term
+parseVar = many1 letter >>= (\v -> return $ Var v)
 
-lC :: Parser LambdaExpr
-lC = term <|> lambdaExprParser
+parseFunction :: Parser Term
+parseFunction = do
+    char '('
+    expr <- lambdaExprParser
+    char ')'
+    return expr
 
---go input = parse (lambdaCalculus input) " "
-test input = parseTest (do expr <- lC; eof; return expr) input
+term :: Parser Term
+term = parseVar <|> parseFunction
+
+identityFunction = "\\x.x"
+test input = parseTest (do expr <- term; eof; return expr) input
 
 -- x = "\\y.y(\\x.x)y"
 
--- SKI Combinator Logic 
+-- SKI Combinator Logic
 
 s :: (a -> b -> c) -> (a -> b) -> a -> c
 s f g x = f x (g x)
@@ -48,6 +61,10 @@ k1 x y = y
 
 i :: a -> a
 i x = x
+
+-- The fixed point Y combinator
+y :: ((a -> a) -> (a -> a)) -> (a -> a)
+y f = f (y f)
 
 compose :: (s -> t) -> (t1 -> s) -> t1 -> t
 compose f g x = f $ g x
