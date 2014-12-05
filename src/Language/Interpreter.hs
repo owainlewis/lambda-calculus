52362c156f7dea2ee027faa973d501804b1105f6
Owain Lewis <owain@owainlewis.com>
2014-12-05 21:02:09 +0000

    env reader

--- src/Language/Interpreter.hs
+++ src/Language/Interpreter.hs
@@ -1,15 +1,21 @@
 module Language.Interpreter
-  ( ) where
+  (  Term(..)
+   , Env(..) ) where
 
 import           Control.Monad.Reader
+import           Data.Monoid          ((<>))
 
-data Term = Apply Term Term | Lambda String Term | Var Term deriving (Show)
+data Term = Apply Term Term | Lambda String Term | Var String deriving (Show)
 
 newtype Env = Env ([(String,Closure)])
 
 type Closure = (Term,Env)
 
 data Value = Lam String Closure | Failure String
+
+instance Show Value where
+  show (Lam x (term, _)) = x <> show term
+  show (Failure f) = show f
 
 interp' :: Term -> Reader Env Value
 --when we have lambda term, we can just return it
@@ -24,10 +30,11 @@ interp' (Var v)
           Nothing -> return . Failure $ "unbound variable: " ++ (show v)
           -- if it is in the environment, than we should interpret it
           Just (term,env) -> local (const env) $ interp' term
---the complicated case is an application
 interp' (Apply t1 t2)
    = do v1 <- interp' t1
         case v1 of
            Failure s -> return (Failure s)
            Lam nv clos -> local (\(Env ls) -> Env ((nv,clos):ls)) $ interp' t2
---I guess not that complicated!
+
+interp :: Term -> Value
+interp term = runReader (interp' term) (Env [])
